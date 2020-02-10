ead5b95377a623b5eb1d136a75aedf5f207356c6
park chanyoung <pcx474@gmail.com>
2020-02-11 01:03:54 +0900

    Initial Commit

---
+++ Dolphin_practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dolphin_practice
+//
+//  Created by 박찬영 on 2020/02/11.
+//  Copyright © 2020 박찬영. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
