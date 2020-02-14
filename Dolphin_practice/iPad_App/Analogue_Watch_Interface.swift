3e1c71c6d1b62e3dbc5f66fd4bd4d34631475eca
park chanyoung <pcx474@gmail.com>
2020-02-14 21:51:29 +0900

    Time Zone step1

--- Dolphin_practice/iPad_App/Analogue_Watch_Interface.swift
+++ Dolphin_practice/iPad_App/Analogue_Watch_Interface.swift
@@ -7,11 +7,38 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct Analogue_Watch_Interface: View {
+    @State var isExpanded : Bool = false
+    private let headerHeight : CGFloat = 100
     
     var body : some View{
-        WatchView()
+        VStack{
+            if !isExpanded{
+                Header().transition(.move(edge:.leading))
+                WatchView(diameter: 170).transition(.scale)
+                SavedTimeZones().transition(.move(edge:.leading))
+                BottomNavBar()
+                .overlay(
+                    ZStack(alignment: .bottom) {
+                        HStack{
+                            if self.isExpanded{
+                                VStack{
+                                    Text("Hello world")
+                                }
+                            }
+                        }.frame(width : self.isExpanded ? UIScreen.main.bounds.width : 50, height : self.isExpanded ? UIScreen.main.bounds.height - headerHeight - sheetTop : 50)
+                            .background(LinearGradient(gradient: Gradient(colors:[Color(.blue),Color(.purple)]), startPoint: .topTrailing, endPoint: .bottomLeading))
+                            .cornerRadius(isExpanded ? 0 : 10)
+                            .clipShape(TopRoundedShape(cornerRadius: isExpanded ? 40 : 10))
+                            .offset(x: isExpanded ? 0 : UIScreen.main.bounds.width / 3, y: isExpanded ? 0 : -50)
+                        
+                        AddButton(isExpanded : self.$isExpanded)
+                    }, alignment: .bottom
+                )
+            }
+        }
     }
     
 }
@@ -22,6 +49,142 @@ struct Analogue_Watch_Interface_Previews: PreviewProvider {
     }
 }
 
+struct TopRoundedShape : Shape {
+    var cornerRadius : CGFloat = 40
+    
+    var animatableData: CGFloat{
+        get{
+            cornerRadius
+        }
+        
+        set{
+            cornerRadius = newValue
+        }
+    }
+    
+    func path(in rect:CGRect) -> Path{
+        var path = Path()
+        path.move(to : CGPoint(x:0,y:cornerRadius))
+        path.addQuadCurve(to: CGPoint(x: cornerRadius,y:0), control: CGPoint.zero)
+        path.addLine(to : CGPoint(x: rect.width - cornerRadius, y:0))
+        path.addQuadCurve(to: CGPoint(x:rect.width,y:cornerRadius), control: CGPoint(x:rect.width, y:0))
+        path.addLine(to: CGPoint(x:rect.width, y: rect.height))
+        path.addLine(to: CGPoint(x:0, y:rect.height))
+        path.closeSubpath()
+        
+        return path
+        
+    }
+}
+struct TimeZone : Identifiable {
+    let id : UUID
+    let Country : String
+    let City : String
+    let time : String
+    
+    static func data() -> [TimeZone]{
+        return[
+            TimeZone(id: UUID(), Country: "USA", City: "Chicago", time: "7:00 AM"),
+            TimeZone(id: UUID(), Country: "Croatia", City: "Zagreb", time: "5:00 AM"),
+            TimeZone(id: UUID(), Country: "Ireland", City: "Limerick", time: "15:00 AM"),
+            TimeZone(id: UUID(), Country: "Burundi", City: "Bujumbura", time: "10:00 AM")
+        ]
+    }
+}
+
+struct TimeZoneView : View {
+    var timeZone = TimeZone.data().first!
+    
+    var body: some View{
+        HStack{
+            VStack(alignment : .leading) {
+                Text(timeZone.Country)
+                    .font(.system(size:15,weight: Font.Weight.black))
+                Text(timeZone.City)
+                    .foregroundColor(.gray)
+            }
+            Spacer()
+            Text(timeZone.time).font(.system(size: 20,weight:Font.Weight.black))
+        }.frame(maxWidth : .infinity).padding(30).background(Color.white).cornerRadius(20).shadow(color: Color.gray.opacity(0.2), radius: 8, x: 0, y: 8).padding(.horizontal)
+            
+        
+    }
+}
+
+struct AddedTimeZoneView : View {
+    var timeZone = TimeZone.data().first!
+    
+    var body : some View{
+        VStack(alignment: .leading){
+            Text(timeZone.Country)
+                .font(.system(size: 15,weight:Font.Weight.black))
+            Text(timeZone.City).foregroundColor(Color.gray)
+            }.padding().background(Color.white).cornerRadius(20)
+            .shadow(color: Color.gray.opacity(0.2), radius: 8, x: 0, y: 0)
+    }
+}
+
+struct Header : View {
+    var headerHeight : CGFloat = 100
+    
+    var body : some View{
+        VStack{
+            HStack{
+                Spacer()
+                Image(systemName: "person.crop.circle")
+                    .resizable().frame(width:30,height:30)
+                    .padding(.horizontal)
+            }
+            Text("Burundi")
+            Text("Sun, 10 Nov 2019")
+        }.frame(height : self.headerHeight)
+    }
+}
+
+struct SavedTimeZones : View {
+    var body : some View {
+        ScrollView(.vertical, showsIndicators: false){
+            VStack{
+                ForEach(TimeZone.data()){ timeZone in
+                    TimeZoneView(timeZone: timeZone)
+                }
+            }
+        }
+    }
+}
+struct BottomNavBar : View {
+    var body : some View{
+        HStack{
+            Image(systemName: "square.grid.2x2").resizable().frame(width:20, height:20)
+            Spacer()
+            Image(systemName: "alarm").resizable().frame(width:20, height:20)
+            Spacer()
+            Image(systemName: "person").resizable().frame(width:20, height:20)
+            Spacer()
+            
+        }.padding(30).frame(maxWidth: .infinity,alignment: .top).background(Color.white)
+        .clipShape(TopRoundedShape(cornerRadius: 30))
+            .shadow(color: Color.gray.opacity(0.2), radius: 10, x: 0, y: -5)
+        
+    }
+}
+struct AddButton : View {
+    @Binding var isExpanded : Bool
+    var body : some View{
+        Button(action : {
+            withAnimation(.spring()) {
+                self.isExpanded.toggle()
+            }
+        }){
+            Image(systemName: "plus")
+            .padding()
+                .background(isExpanded ? Color.white : Color.clear)
+                .foregroundColor(isExpanded ? Color.black : Color.white)
+                .cornerRadius(isExpanded ? 25 : 0)
+                .rotationEffect(Angle(degrees: isExpanded ? 45 : 90), anchor: .center)
+        }.offset(x:isExpanded ? 0 : UIScreen.main.bounds.width / 3, y: -50 )
+    }
+}
 struct WatchView : View{
     var diameter : CGFloat = 200
     var smallCapsuleHeight : CGFloat = 5
